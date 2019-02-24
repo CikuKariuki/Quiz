cb3854a92c116ad78cb5471d63d32f9e1bfa1c58
Ciku Kariuki <wanjikukariuki03@gmail.com>
2019-02-24 23:19:03 +0300

    made top buttons

---
+++ trial.js
@@ -0,0 +1,164 @@
+$(document).ready(function() {
+  $("#hq").click(function(){
+    $("#quecss").hide();
+    $("#quejs").hide();
+    $("#quehtml").slideToggle();
+  });
+
+  $("#cq").click(function(){
+    $("#quehtml").hide()
+    $("#quejs").hide()
+    $("#quecss").slideToggle()
+  });
+
+  $("#jq").click(function(){
+    $("#quecss").hide()
+    $("#quehtml").hide()
+    $("#quejs").slideToggle()
+
+  });
+ });
+
+ $(document).ready(function() {
+  $("#submit1").click(function(){
+    alert("sasa")
+    });
+  $("#submit2").click(function(){
+    alert("poa")
+  });
+
+  $("#submit3").click(function(){
+    alert("poa sana")
+  });
+
+  event.preventDefault();
+});
+
+
+
+
+
+
+
+
+$(document).ready(function() {
+  $("#hq").click(function(){
+    $("#quecss").hide();
+    $("#quejs").hide();
+    $("#quehtml").slideToggle();
+  });
+
+  $("#cq").click(function(){
+    $("#quehtml").hide()
+    $("#quejs").hide()
+    $("#quecss").slideToggle()
+  });
+
+  $("#jq").click(function(){
+    $("#quecss").hide()
+    $("#quehtml").hide()
+    $("#quejs").slideToggle()
+
+  });
+ });
+
+ $(document).ready(function() {
+     $("#submit1").click(function(){
+    var marks = 0;
+    var score = 0;
+    var ques1 = document.forms["quehtml"]["htmlq1"].value;
+    var ques2 = document.forms["quehtml"]["htmlq2"].value;
+    var ques3 = document.forms["quehtml"]["htmlq3"].value;
+    var ques4 = document.forms["quehtml"]["htmlq4"].value;
+    var ques5 = document.forms["quehtml"]["htmlq5"].value;
+
+    for(i=0; i<=5; 1++){
+      if(ques1 == "20"){
+        score = marks + 20;
+      }
+      if(ques2 == "20"){
+        score = score+20;
+      }
+    //   if(ques3 == "20"){
+    //     score = score + 20;
+    //   }
+    //   if(ques4 == "20"){
+    //     score = score + 20;
+    //   }
+    //   if(ques5 == "20"){
+    //     score = score + 20;
+    //   }
+    // } else {
+    //   alert("answer all questions")
+    // }
+alert(score);
+    // document.getElementById("message").innerHTML="You scored" + " " + score +"%".
+    });
+  $("#submit2").click(function(){
+    var marks = 0;
+    var score = 0;
+    var ques1 = document.forms["quecss"]["cssq1"].value;
+    var ques2 = document.forms["quecss"]["cssq2"].value;
+    var ques3 = document.forms["quecss"]["cssq3"].value;
+    var ques4 = document.forms["quecss"]["cssq4"].value;
+    var ques5 = document.forms["quecss"]["cssq5"].value;
+
+    for(i=0; i<=5; 1++){
+      if(ques1 == "20"){
+        score = marks + 20;
+      }
+      if(ques2 == "20"){
+        score = score+20;
+      }
+    //   if(ques3 == "20"){
+    //     score = score + 20;
+    //   }
+    //   if(ques4 == "20"){
+    //     score = score + 20;
+    //   }
+    //   if(ques5 == "20"){
+    //     score = score + 20;
+    //   }
+    // } else {
+    //   alert("answer all questions")
+    // }
+alert(score);
+    // document.getElementById("message").innerHTML="You scored" + " " + score +"%".
+
+  });
+
+  $("#submit3").click(function(){
+    var marks = 0;
+    var score = 0;
+    var ques1 = document.forms["quejs"]["jsq1"].value;
+    var ques2 = document.forms["quejs"]["jsq2"].value;
+    var ques3 = document.forms["quejs"]["jsq3"].value;
+    var ques4 = document.forms["quejs"]["jsq4"].value;
+    var ques5 = document.forms["quejs"]["jsq5"].value;
+
+    for(i=0; i<=5; 1++){
+      if(ques1 == "20"){
+        score = marks + 20;
+      }
+      if(ques2 == "20"){
+        score = score+20;
+      }
+    //   if(ques3 == "20"){
+    //     score = score + 20;
+    //   }
+    //   if(ques4 == "20"){
+    //     score = score + 20;
+    //   }
+    //   if(ques5 == "20"){
+    //     score = score + 20;
+    //   }
+    // } else {
+    //   alert("answer all questions")
+    // }
+alert(score);
+    // document.getElementById("message").innerHTML="You scored" + " " + score +"%".
+
+  });
+
+  event.preventDefault();
+});
